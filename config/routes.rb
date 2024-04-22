bb330dba20f3879fd21d732f28e79597196b2ba2
McKenna Noakes <mckenna.noakes@outlook.com>
2024-04-22 15:43:21 -0500

    double fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
-<<<<<<< HEAD
   get 'avery0123', to: 'pages#avery0123', as: 'avery0123'
-=======
-  
-  
-  get 'heygo67', to: 'pages#heygo67', as: 'heygo67'
-
->>>>>>> 1f4c668cdd3bf2b6a01a9988b7899989dab6e3e5
   get 'danny080298', to: 'pages#danny080298', as: 'danny080298'
   get 'yahianahhas', to: 'pages#yahianahhas', as: 'yahianahhas'
   get 'heygo67', to: 'pages#heygo67', as: 'heygo67'
